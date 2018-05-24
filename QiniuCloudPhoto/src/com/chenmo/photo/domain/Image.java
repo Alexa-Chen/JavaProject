cea6b3e913c64c47510402d2b06dc05cabc59917
Alexa-Chen <wwwchenyuhan@qq.com>
2018-05-24 23:52:52 +0800

    java

---
+++ QiniuCloudPhoto/src/com/chenmo/photo/domain/Image.java
@@ -0,0 +1,53 @@
+package com.chenmo.photo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Image implements Serializable {
+
+    private int id;
+    private String name;
+    private String url;
+    private Date date;
+    private User user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
